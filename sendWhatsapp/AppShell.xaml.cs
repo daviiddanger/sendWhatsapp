9f61bcf75714e60f8f5f0a4f2e59e49e13392840
NeoDanger <NeoDanger@NEO>
2023-09-28 18:57:44 -0600

    Agregar archivos de proyecto.

---
+++ sendWhatsapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sendWhatsapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ sendWhatsapp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace sendWhatsapp
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string Numero = ladaEntry.Text + numberEntry.Text;
+            EnviarWhatsapp(Numero, messageEntry.Text);
+          
+        }
+
+        public async static void EnviarWhatsapp(string numTelefonico, string Mensaje)
+        {
+            bool sepodra = await Launcher.Default.CanOpenAsync($"whatsapp://send?phone=+{numTelefonico}&text={Mensaje}");
+           if ( sepodra )
+            {
+                await Launcher.Default.OpenAsync($"whatsapp://send?phone=+{numTelefonico}&text={Mensaje}");
+
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "No se puede abrir WhatsApp.", "OK");
+
+            }
+ 
+             
+
+
+
+
+
+            
+        }
+    }
+}
